b0ed89caec4ee744ca32709ab6d866bee2052690
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-07-27 10:35:03 -0600

    test for second and third show page story complete

--- spec/features/user_sees_a_trip_show_page_spec.rb
+++ spec/features/user_sees_a_trip_show_page_spec.rb
@@ -16,5 +16,17 @@ describe "a user" do
       expect(page).to have_content(trail_2.address)
       expect(page).to have_content(trail_2.length)
     end
+    it 'sees trips with total trails distance' do
+      trip = Trip.create(name: 'A long and winding road', start_date: Date.today, end_date: Date.today)
+      trail_1 = trip.trails.create(name: 'Trail by Fire', address: 'somewhere', length: 20)
+      trail_2 = trip.trails.create(name: 'Trail by Combat', address: 'somewhere', length: 10)
+      expected_total = 30
+      expected_average = 15
+
+      visit trip_path(trip)
+
+      expect(page).to have_content(expected_total)
+      expect(page).to have_content(expected_average)
+    end
   end
 end

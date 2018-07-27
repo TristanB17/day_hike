e621383350b43565014fab11e0f1adadf70e44a3
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-07-27 10:43:26 -0600

    longest and shortest methods tests made

--- spec/features/user_sees_a_trip_show_page_spec.rb
+++ spec/features/user_sees_a_trip_show_page_spec.rb
@@ -16,7 +16,7 @@ describe "a user" do
       expect(page).to have_content(trail_2.address)
       expect(page).to have_content(trail_2.length)
     end
-    it 'sees trips with total trails distance' do
+    it 'sees trips with total and average trail distance' do
       trip = Trip.create(name: 'A long and winding road', start_date: Date.today, end_date: Date.today)
       trail_1 = trip.trails.create(name: 'Trail by Fire', address: 'somewhere', length: 20)
       trail_2 = trip.trails.create(name: 'Trail by Combat', address: 'somewhere', length: 10)
@@ -27,6 +27,18 @@ describe "a user" do
 
       expect(page).to have_content(expected_total)
       expect(page).to have_content(expected_average)
+    end
+    it 'sees longest and shortest trails' do
+      trip = Trip.create(name: 'A long and winding road', start_date: Date.today, end_date: Date.today)
+      trail_1 = trip.trails.create(name: 'Trail by Fire', address: 'somewhere', length: 20)
+      trail_2 = trip.trails.create(name: 'Trail by Combat', address: 'somewhere', length: 10)
+      expected_total = 30
+      expected_average = 15
+
+      visit trip_path(trip)
+
+      expect(page).to have_content("Longest Distance: #{trail_1.length}")
+      expect(page).to have_content("Shortest Distance: #{trail_2.length}")
     end
   end
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Trip, type: :model do
   describe 'calculations' do
-    it '#total_distance' do
+    it '#total_distance + #total_average' do
       trip = Trip.create(name: 'A long and winding road', start_date: Date.today, end_date: Date.today)
       trail_1 = trip.trails.create(name: 'Trail by Fire', address: 'somewhere', length: 20)
       trail_2 = trip.trails.create(name: 'Trail by Combat', address: 'somewhere', length: 10)
@@ -11,6 +11,16 @@ describe Trip, type: :model do
 
       expect(trip.total_distance).to eq(expected_total)
       expect(trip.average_distance).to eq(expected_average)
+    end
+    it '#longest_trail + #shortest_trail' do
+      trip = Trip.create(name: 'A long and winding road', start_date: Date.today, end_date: Date.today)
+      trail_1 = trip.trails.create(name: 'Trail by Fire', address: 'somewhere', length: 40)
+      trail_2 = trip.trails.create(name: 'Trail by Combat', address: 'somewhere', length: 15)
+      expected_longest = 40
+      expected_average = 15
+
+      expect(trip.longest_trail).to eq(expected_longest)
+      expect(trip.shortest_trail).to eq(expected_shortest)
     end
   end
 end

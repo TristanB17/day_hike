1c5bf08bc72a08722ed1aebd90b43c0d0d5efd69
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-07-27 10:21:34 -0600

    second test passing

--- spec/features/user_sees_a_trip_index_spec.rb
+++ spec/features/user_sees_a_trip_index_spec.rb
@@ -15,5 +15,18 @@ describe "a user" do
       expect(page).to have_content(trip_2.start_date)
       expect(page).to have_content(trip_2.end_date)
     end
+    it 'sees a single trip with all trip info' do
+      trip_1 = Trip.create(name: 'A long and winding road', start_date: Date.today, end_date: Date.today)
+      trip_2 = Trip.create(name: 'A song and finding road', start_date: Date.today, end_date: Date.today)
+
+      visit trips_path
+
+      click_link trip_1.name
+
+      expect(current_path).to eq(trip_path(trip_1))
+      expect(page).to have_content(trip_1.name)
+      expect(page).to have_content(trip_1.start_date)
+      expect(page).to have_content(trip_1.end_date)
+    end
   end
 end
